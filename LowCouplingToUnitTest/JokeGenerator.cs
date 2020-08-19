0ec14462f37181f4f5f4146833fe0953d8f0eb69
GET Academy <terje@kolderup.net>
2020-08-19 09:20:46 +0200

    Initial commit

---
+++ LowCouplingToUnitTest/JokeGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace LowCouplingToUnitTest
+{
+    class JokePairWhichStartsWithTheSameWordGenerator
+    {
+        public async Task<string> GetJoke(string word)
+        {
+            var client = new RestClient("https://api.chucknorris.io");
+            var request = new RestRequest($"/jokes/search?query={word}", DataFormat.Json);
+            var result = await client.GetAsync<ChuckNorrisJokeSearchResultSet>(request);
+
+            foreach (var jokeObj in result.result)
+            {
+                var joke= jokeObj.value;
+                var firstPosition = joke.IndexOf(word, StringComparison.OrdinalIgnoreCase);
+                if (firstPosition == -1) continue;
+                var secondPosition = joke.IndexOf(word, firstPosition + 1, StringComparison.OrdinalIgnoreCase);
+                if (secondPosition != -1) return joke;
+            }
+
+            return null;
+        }
+    }
+}
