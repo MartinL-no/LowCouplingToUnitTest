30c3a0675f172554772a43eb68155a90d1fb08e2
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-04-13 10:18:53 +0200

    add moq tests

--- LowCouplingToUnitTest.UnitTest/JokeGeneratorTest.cs
+++ LowCouplingToUnitTest.UnitTest/JokeGeneratorTest.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Moq;
 using NUnit.Framework;
 
 namespace LowCouplingToUnitTest.UnitTest
@@ -39,6 +41,51 @@ namespace LowCouplingToUnitTest.UnitTest
             var wrongJoke = $"Joke where {word} is used once";
             var dummyClient = new DummyClient(expectedJoke, wrongJoke);
             var jokeGenerator = new JokeGenerator(dummyClient);
+
+            var joke = await jokeGenerator.GetJoke(word);
+
+            Assert.That(joke, Is.EqualTo(expectedJoke));
+            Assert.That(joke, Is.Not.EqualTo(wrongJoke));
+        }
+
+        [Test]
+        public async Task JokeGeneratorTestOneJokeWithMoq()
+        {
+            var word = "pizza";
+            var expectedJoke = "Chuck Norris once whent to pizzahut. He ordered an eggroll with some swiss cheese. A rotten cow corpse.A dead president.And a large box with Hellfire missiles. Pizza hut served it on a big tuna fish pizza. Chuck Norris Roundhousekicked the whole place to oblivion making everyone inside suffer terrible pain for eternity. Chuck Norris did not order any pizza.";
+            var expectedListReturned = new List<string>
+            {
+                expectedJoke
+            };
+            var clientMock = new Mock<IClient>();
+            clientMock
+                .Setup(c => c.GetResult(It.IsAny<string>()))
+                .ReturnsAsync(expectedListReturned);
+            var jokeGenerator = new JokeGenerator(clientMock.Object);
+
+            var joke = await jokeGenerator.GetJoke(word);
+
+            Assert.That(joke, Is.Not.Null);
+            Assert.That(joke, Is.TypeOf(typeof(string)));
+            Assert.That(joke, Is.EqualTo(expectedJoke));
+        }
+
+        [Test]
+        public async Task JokeGeneratorTestTwoJokesWithMoq()
+        {
+            var word = "pizza";
+            var expectedJoke = "Chuck Norris once whent to pizzahut. He ordered an eggroll with some swiss cheese. A rotten cow corpse.A dead president.And a large box with Hellfire missiles. Pizza hut served it on a big tuna fish pizza. Chuck Norris Roundhousekicked the whole place to oblivion making everyone inside suffer terrible pain for eternity. Chuck Norris did not order any pizza.";
+            var wrongJoke = $"Joke where {word} is used once";
+            var expectedListReturned = new List<string>
+            {
+                expectedJoke,
+                wrongJoke
+            };
+            var clientMock = new Mock<IClient>();
+            clientMock
+                .Setup(c => c.GetResult(It.IsAny<string>()))
+                .ReturnsAsync(expectedListReturned);
+            var jokeGenerator = new JokeGenerator(clientMock.Object);
 
             var joke = await jokeGenerator.GetJoke(word);
 
